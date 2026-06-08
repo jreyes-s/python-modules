d87f67a2d76f24ce5e47d5a9d3cfcc20cc70f517
jreyes-s <jreyes-s@student.42madrid.com>
2026-06-08 04:32:16 +0200

    updates

--- python02/ex0/ft_first_exception.py
+++ python02/ex0/ft_first_exception.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 
-def input_temperature(temp_str) -> int:
+def input_temperature(temp_str: str) -> int:
     return int(temp_str)
 
 
 def test_temperature() -> None:
-    print("Garden Temperature ===")
+    print("=== Garden Temperature ===")
     data = ["25", "abc"]
 
     for d in data:
-        print(f"\nInput data is '{d}'")
+        print(f"Input data is '{d}'")
+        print()
         try:
             temperature = input_temperature(d)
             print(f"Temperature is now {temperature}°C")
         except ValueError as e:
             print(f"Caught input_temperature error: {e}")
-    print("\nAll tests completed - program didn't crash!")
+    print()
+    print("All tests completed - program didn't crash!")
 
 
 if __name__ == "__main__":
--- python02/ex1/ft_raise_exception.py
+++ python02/ex1/ft_raise_exception.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python3
 
-def input_temperature(temp_str) -> int:
+def input_temperature(temp_str: str) -> int:
     t_int = int(temp_str)
 
     if t_int < 0:
-        raise ValueError(f"{t_int}°C is too cold for plants (min. 0°C)")
+        raise ValueError(f"{t_int}°C is too cold for plants (min 0°C)")
     elif t_int > 40:
-        raise ValueError(f"{t_int}°C is too hot for plants (max. 40°C)")
+        raise ValueError(f"{t_int}°C is too hot for plants (max 40°C)")
     return t_int
 
 
-def test_temperature(temp_str) -> None:
+def test_temperature() -> None:
     print("=== Garden Temperature Checker ===")
+    temp_str: list[str] = ["25", "abc", "100", "-50"]
     for data in temp_str:
-        print(f"\nInput data is '{data}'")
+        print()
+        print(f"Input data is '{data}'")
         try:
             temp = input_temperature(data)
             print(f"Temperature is now {temp}°C")
         except ValueError as e:
             print(f"Caught input_temperature error: {e}")
-    print("\nAll tests completed - program didn't crash!")
+    print()
+    print("All tests completed - program didn't crash!")
 
 
 if __name__ == "__main__":
-    test_temperature(["25", "abc", "100", "-50"])
+    test_temperature()
