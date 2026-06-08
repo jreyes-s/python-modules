1ba6791a9322ef7f1f655967f38f52b6e720316f
jreyes-s <jreyes-s@student.42madrid.com>
2026-06-09 01:38:05 +0200

    update

--- python03/ex1/ft_score_analytics.py
+++ python03/ex1/ft_score_analytics.py
@@ -3,13 +3,13 @@
 import sys
 
 
-def ft_score_analytics(score: list) -> None:
-    total_players: int = len(score)
-    total_score: int = sum(score)
-    average_score: float = round(total_score / total_players, 1)
-    high_score: int = max(score)
-    low_score: int = min(score)
-    score_range: int = high_score - low_score
+def ft_score_analytics(score: list[int]) -> None:
+    total_players = len(score)
+    total_score = sum(score)
+    average_score = total_score / total_players
+    high_score = max(score)
+    low_score = min(score)
+    score_range = high_score - low_score
 
     print(f"Scores processed: {score}")
     print(f"Total players: {total_players}")
@@ -18,28 +18,26 @@ def ft_score_analytics(score: list) -> None:
     print(f"High score: {high_score}")
     print(f"Low score: {low_score}")
     print(f"Score range: {score_range}")
-    print("\n")
+    print()
 
 
-def main() -> None:
+if __name__ == "__main__":
+    print("=== Player Score Analytics ===")
     if len(sys.argv) == 1:
-        print("No score provided. Usage: python3 "
-              "ft_score_analytics.py <score1> <score2> ...\n")
-        return
+        print("No scores provided. Usage: python3 "
+              "ft_score_analytics.py <score1> <score2> ...")
+        print()
+        sys.exit(1)
 
-    clean_scores = []
+    clean_scores: list[int] = []
     for arg in sys.argv[1:]:
         try:
             clean_scores += [int(arg)]
         except ValueError:
             print(f"Invalid parameter: '{arg}'")
     if not clean_scores:
-        print("No score provided. Usage: python3 "
-              "ft_score_analytics.py <score1> <score2> ...\n")
+        print("No scores provided. Usage: python3 "
+              "ft_score_analytics.py <score1> <score2> ...")
+        print()
     else:
         ft_score_analytics(clean_scores)
-
-
-if __name__ == "__main__":
-    print("=== Player Score Analytics ===")
-    main()
--- python03/ex2/ft_coordinate_system.py
+++ python03/ex2/ft_coordinate_system.py
@@ -5,17 +5,18 @@ import math
 
 def get_player_pos() -> tuple[float, ...]:
     while True:
-        data = input("Enter new coordinates as floats in format 'x,y,z': ")
-        new_data = data.split(",")
-        if len(new_data) != 3:
+        user_input = input(
+            "Enter new coordinates as floats in format 'x,y,z': ")
+        parts = user_input.split(",")
+        if len(parts) != 3:
             print("Invalid syntax")
             continue
         tp = []
-        for x in new_data:
+        for part in parts:
             try:
-                tp.append(float(x.strip()))
+                tp.append(float(part.strip()))
             except ValueError as e:
-                print(f"Error on parameter '{x}': {e}")
+                print(f"Error on parameter '{part}': {e}")
                 break
         else:
             return tuple(tp)
@@ -28,15 +29,16 @@ def main() -> None:
     distance1 = round(math.sqrt(x1**2 + y1**2 + z1**2), 4)
     print(f"Got a first tuple: {pos1}")
     print(f"It includes: X={x1}, Y={y1}, Z={z1}")
-    print(f"Distance to center: {distance1}\n")
+    print(f"Distance to center: {distance1}")
 
-    print("\nGet a second set of coordinates")
+    print("Get a second set of coordinates")
     pos2 = get_player_pos()
     x2, y2, z2 = pos2
-    dist_between = round(math.sqrt((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2), 4)
-    print(f"Distance between the 2 sets of coordinates: {dist_between}")
+    distance2 = round(
+        math.sqrt((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2), 4)
+    print(f"Distance between the 2 sets of coordinates: {distance2}")
 
 
 if __name__ == "__main__":
-    print("=== Game coordinate system ===")
+    print("=== Game Coordinate System ===")
     main()
