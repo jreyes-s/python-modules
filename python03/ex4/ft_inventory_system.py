2d252d98e80000285044095bf5a2e3f9cda1072e
jreyes-s <jreyes-s@student.42madrid.com>
2026-06-11 00:06:43 +0200

    done

--- python03/ex4/ft_inventory_system.py
+++ python03/ex4/ft_inventory_system.py
@@ -3,69 +3,61 @@
 import sys
 
 
-def check_duplicates(args: list) -> None:
-    keys = [arg.split(':')[0] for arg in args if ':' in arg]
-
-    seen = set()
-    duplicates = set()
-    for key in keys:
-        if key in seen:
-            duplicates.add(key)
-        seen.add(key)
-
-    for dup in duplicates:
-        print(f"Redundant item '{dup}' - discarding")
-
-
-def percentage(dict_new: dict, quantity: int) -> None:
-    for k, v in dict_new.items():
-        percent = round((v * 100) / quantity, 1)
-        print(f"Item {k} presents {percent}%")
-
-
 def main() -> None:
-    args: list = sys.argv[1:]
-    dict_new: dict = {}
-    new_list: list = list()
-    items_quantity: int = 0
+    print("=== Inventory System Analysis ===")
 
-    if not args:
-        return
+    arguments: list[str] = sys.argv[1:]
+    inventory: dict[str, int] = dict()
 
-    try:
-        check_duplicates(args)
-        for arg in args:
-            if ':' in arg:
-                key, value_str = arg.split(':')
-                value = int(value_str)
+    """ Check if arg contains ':' """
+    for argument in arguments:
+        if ":" not in argument or argument[-1] == ":":
+            print(f"Error - invalid parameter '{argument}'")
+            continue
 
-                if key not in dict_new:
-                    dict_new[key] = value
-                    new_list.append(arg.split(':')[0])
-            if ':' not in arg:
-                print(f"Error - Invalid parameter '{arg}'")
-    except ValueError as e:
-        print(f"Quantity error for '{arg.split(':')[0]}': {e}")
-    except IndexError as e:
-        print(f"IndexError: {e}")
+        item_name, text_quantity = argument.split(":", 1)
 
-    print(f"Got inventory: {dict_new}")
-    print(f"Item list: {new_list}")
+        """ Check for duplicates """
+        if item_name in inventory:
+            print(f"Redundant item '{item_name}' - discarding")
+            continue
 
-    items_quantity = sum(dict_new.values())
-    print(f"Total quantity of the {len(dict_new)} items: {items_quantity}")
+        try:
+            quantity = int(text_quantity)
+            inventory[item_name] = quantity
+        except ValueError as error:
+            print(f"Quantity error for '{text_quantity}': {error}")
+            continue
+    print(f"Got inventory: {inventory}")
 
-    percentage(dict_new, items_quantity)
-    winner_name, winner_qty = max(dict_new.items(), key=lambda x: x[1])
-    loser_name, loser_qty = min(dict_new.items(), key=lambda x: x[1])
+    item_list: list[str] = list(inventory.keys())
+    print(f"Item list: {item_list}")
 
-    print(f"Item most abundant: {winner_name} with quantity {winner_qty}")
-    print(f"Item least abundant: {loser_name} with quantity {loser_qty}")
+    total_items = len(inventory)
+    total_quantity = sum(inventory.values())
+    print(f"Total quantity of the {total_items} items: {total_quantity}")
 
-    dict_new.update({'magic item': 1})
-    print(f"Updated inventory: {dict_new}")
+    most_abundant_item, most_abundant_qty = max(
+        inventory.items(), key=lambda x: x[1])
+    least_abundant_item, least_abundant_qty = min(
+        inventory.items(), key=lambda x: x[1])
+
+    for item in item_list:
+        qty = inventory[item]
+        percentage = (qty / total_quantity *
+                      100) if total_quantity > 0 else 0.0
+        print(f"Item {item} represents {round(percentage, 1)}%")
+
+    print(
+        f"Item most abundant: {most_abundant_item} with quantity "
+        f"{most_abundant_qty}")
+    print(
+        f"Item least abundant: {least_abundant_item} with quantity "
+        f"{least_abundant_qty}")
+
+    inventory.update({"magic_item": 1})
+    print(f"Updated inventory: {inventory}")
 
 
 if __name__ == "__main__":
-    print("=== Inventory System Analysis ===")
     main()
